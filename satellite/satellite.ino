939efebfd1a1c4d7f09b46b66caa10516cc8a28f
Vaclav Pavlicek <venda.pavlicek@gmail.com>
2020-12-26 18:21:36 +0100

    Refactor sensors init.

--- satellite/satellite.ino
+++ satellite/satellite.ino
@@ -31,6 +31,7 @@
 #define sd_cs_pin 35
 
 #define D13_led_pin 42
+#define Serial SerialUSB
 
 const int UV_SENSOR_PIN = A1;
 const int OXYGEN_SENSOR_PIN = A3;
@@ -38,6 +39,8 @@ const int OXYGEN_SENSOR_PIN = A3;
 const int TIME_OF_MEASUREMENT = 280;
 const int TIME_OF_EQUALITY = 40;
 const int TIME_OF_SLEEP = 9680;
+
+const int SERIAL_PORT = 57600;
 
 const float VRefer = 5;
 
@@ -53,11 +56,7 @@ SCD30 airSensor;
 CCS811 myCCS811(CCS811_ADDR);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 AS7265X spectroscope;
-
-#define Serial SerialUSB
-
 MPU9250 IMU(Wire,0x68);
-int status;
 
 uint32_t messageId;
 uint8_t messageType;
@@ -144,6 +143,7 @@ float w;
 float l;
 
 bool isRadioOk = true;
+bool isSDCardInitialised = true;
 
 String filename;
 
@@ -154,7 +154,6 @@ uint8_t hour;
 uint8_t minute;
 uint8_t second;
 
-#define Serial SerialUSB
 bool debugLog = true;
 
 float voltage_shunt;
@@ -162,87 +161,35 @@ float voltage_bus;
 float current_mA;
 float voltage_load;
 
-
 void setup() {
-  Serial.begin(57600);
+  delay(10000);
+  Serial.begin(SERIAL_PORT);
+  
+  if (debugLog) {
+    Serial.println("--------------------   Setup start   --------------------");
+  }
 
   Wire.begin();
 
-  gps.begin();
-  //gps.debugPrintOn(57600);
-  
-  if (!bme.begin(BME280_ADRESS) && debugLog) {
-    Serial.println("External BME280 not found!");
-  }
-  
-  if (!bme_cansat.begin(BME280_ADDRESS_OPEN_CANSAT) && debugLog) {
-    Serial.println("Internal BME280 not found!");
-  }
-  
+  initGPS();
+  initExternalBME();
+  initInternalBME();
+  initAirSensor();
+  initCCS811();
+  initIMU();
+  initRadio();
+  initAdafruitMLX();
+  initSpectroscope();
+  initSDCard();
+
   lightMeter.begin();
 
   ina219.begin();
 
-  airSensor.begin();
-
-  CCS811Core::status returnCode = myCCS811.begin();
-  Serial.println("CCS811 begin exited with: " + String(returnCode));
-
-  status = IMU.begin();
-  if (status < 0 && debugLog) {
-    Serial.println("IMU initialization unsuccessful");
-    Serial.println("Check IMU wiring or try cycling power");
-  }
-
-  Serial.println("FREQUENCY");
-  if(!radio.initialize(FREQUENCY, MYNODEID, NETWORKID))
-  {
-    Serial.println("init");
-    isRadioOk = false;
-    if (debugLog) {
-      Serial.println("RFM69HW initialization failed!"); 
-    }
-  }
-  else
-  {
-    Serial.println("error");
-    radio.setFrequency(FREQUENCYSPECIFIC);
-    radio.setHighPower(true);
-    if (debugLog) {
-      Serial.println("RFM69HW successful!"); 
-    }
-  }
-
-  if (!SD.begin(sd_cs_pin) && debugLog) {
-    Serial.println("initialization failed!");
-  } else if (debugLog) {   
-    Serial.println("initialization done."); 
-  }
-
-  mlx.begin();
-
-  if(spectroscope.begin() == false)
-  {
-    Serial.println("Spectroscope does not appear to be connected.");
-  }
-
-  filename = "result.csv";
-  file = SD.open(filename, FILE_WRITE);
- 
-  if (file) {
-    file.print("message;light;uvIndex;tempCanSat;tempMPU;tempExternal;tempSCD30;ambientTemp;");
-    file.print("objectTemp;humCanSat;humExternal;humSCD30;pressCanSat;pressExternal;");
-    file.print("altCanSat;altExternal;accX;accY;accZ;");
-    file.print("rotX;rotY;rotZ;magX;magY;magZ;year;month;day;");
-    file.print("hour;minute;second;numOfSats;latInt;lonInt;latAfterDot;");
-    file.print("lonAfterDot;voltage_shunt;voltage_bus;current_mA;");
-    file.println("voltage_load;co2SCD30;co2CCS811;tvoc;o2Con;a;b;c;d;e;f;g;h;i;j;k;l;r;s;t;u;v;w;");
-    file.close();
-    if (debugLog) {     
-      Serial.println("File header written."); 
-    }
+  if (isSDCardInitialised) {
+    writeFileHeader();
   } else if (debugLog) {
-    Serial.println("Error writing file header.");
+    Serial.println("File header could not be written because SD card is not initialised!");
   }
 
   pinMode(D13_led_pin, OUTPUT);
@@ -250,6 +197,10 @@ void setup() {
   messageId = 0;
   messageType = 1;
   messageCounter = 0;
+  
+  if (debugLog) {
+    Serial.println("--------------------    Setup end    --------------------");
+  }
 }
 
 void loop() {
@@ -386,26 +337,26 @@ void loop() {
   } else {
     data4.messageType = 4;
     data4.messageId = messageId;
-    //spectroscope.takeMeasurements();
+    spectroscope.takeMeasurements();
 
-    data4.a = 1.0;//spectroscope.getCalibratedA();
-    data4.b = 2.0;//spectroscope.getCalibratedB();
-    data4.c = 3.0;//spectroscope.getCalibratedC();
-    data4.d = 4.0;//spectroscope.getCalibratedD();
-    data4.e = 5.0;//spectroscope.getCalibratedE();
-    data4.f = 6.0;//spectroscope.getCalibratedF();
-    data4.g = 7.0;//spectroscope.getCalibratedG();
-    data4.h = 8.0;//spectroscope.getCalibratedH();
-    data4.i = 9.0;//spectroscope.getCalibratedI();
-    data4.j = 10.0;//spectroscope.getCalibratedJ();
-    /*k = spectroscope.getCalibratedK();
-    l = spectroscope.getCalibratedL();*/
-    data4.r = 11.0;//spectroscope.getCalibratedR();
-    data4.s = 12.0;//spectroscope.getCalibratedS();
-    data4.t = 13.0;//spectroscope.getCalibratedT();
-    /*u = spectroscope.getCalibratedU();
+    data4.a = spectroscope.getCalibratedA();
+    data4.b = spectroscope.getCalibratedB();
+    data4.c = spectroscope.getCalibratedC();
+    data4.d = spectroscope.getCalibratedD();
+    data4.e = spectroscope.getCalibratedE();
+    data4.f = spectroscope.getCalibratedF();
+    data4.g = spectroscope.getCalibratedG();
+    data4.h = spectroscope.getCalibratedH();
+    data4.i = spectroscope.getCalibratedI();
+    data4.j = spectroscope.getCalibratedJ();
+    k = spectroscope.getCalibratedK();
+    l = spectroscope.getCalibratedL();
+    data4.r = spectroscope.getCalibratedR();
+    data4.s = spectroscope.getCalibratedS();
+    data4.t = spectroscope.getCalibratedT();
+    u = spectroscope.getCalibratedU();
     v = spectroscope.getCalibratedV();
-    w = spectroscope.getCalibratedW();*/
+    w = spectroscope.getCalibratedW();
   
     if(isRadioOk)
     {
@@ -566,5 +517,134 @@ void printSensorError()
     if (error & 1 << 1) Serial.print("ReadRegInvalid");
     if (error & 1 << 0) Serial.print("MsgInvalid");
     Serial.println();
+  }
+}
+
+void initGPS() {
+  gps.begin();
+
+  if (debugLog) {
+    gps.debugPrintOn(SERIAL_PORT);
+  }
+}
+
+void initExternalBME() {
+  bool initialised = bme.begin(BME280_ADRESS);
+  
+  if (initialised && debugLog) {
+    Serial.println("External BME280 initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("External BME280 initialisation failed!");
+  }
+}
+
+void initInternalBME() {
+  bool initialised = bme_cansat.begin(BME280_ADDRESS_OPEN_CANSAT);
+
+  if (initialised && debugLog) {
+    Serial.println("Internal BME280 initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("Internal BME280 initialisation failed!");
+  }
+}
+
+void initAirSensor() {
+  bool initialised = airSensor.begin();
+
+  if (initialised && debugLog) {
+    Serial.println("SCD30 sensor initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("SCD30 sensor initialisation failed!");
+  }
+}
+
+void initCCS811() {
+  bool initialised = myCCS811.begin();
+
+  if (initialised && debugLog) {
+    Serial.println("CCS811 sensor initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("CCS811 sensor initialisation failed!");
+  }
+}
+
+void initIMU() {
+  int status = IMU.begin();
+
+  if (status < 0 && debugLog) {
+    Serial.println("IMU initialisation unsuccessful.");
+  } else if (debugLog) {
+    Serial.println("IMU initialisation successful!");
+  }
+}
+
+void initRadio() {
+  bool initialised = radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+
+  if (!initialised) {
+    radio.setFrequency(FREQUENCYSPECIFIC);
+    radio.setHighPower(true);
+
+    if (debugLog) {
+      Serial.println("RFM69HW initialisation successful."); 
+    }
+  } else {
+    isRadioOk = false;
+
+    if (debugLog) {
+      Serial.println("RFM69HW initialisation failed!"); 
+    }
+  }
+}
+
+void initSDCard() {
+  isSDCardInitialised = SD.begin(sd_cs_pin);
+
+  if (isSDCardInitialised && debugLog) {
+    Serial.println("SD card initialisation successful.");
+  } else if (debugLog) {   
+    Serial.println("SD card initialisation failed!"); 
+  }
+}
+
+void initAdafruitMLX() {
+  bool initialised = mlx.begin();
+
+  if (initialised && debugLog) {
+    Serial.println("Adafruit MLX initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("Adafruit MLX initialisation failed!");
+  }
+}
+
+void initSpectroscope() {
+  bool initialised = spectroscope.begin();
+
+  if (initialised && debugLog) {
+    Serial.println("Spectroscope initialisation successful.");
+  } else if (debugLog) {
+    Serial.println("Spectroscope initialisation failed!");
+  }
+}
+
+void writeFileHeader() {
+  filename = "result.csv";
+  file = SD.open(filename, FILE_WRITE);
+ 
+  if (file) {
+    file.print("message;light;uvIndex;tempCanSat;tempMPU;tempExternal;tempSCD30;ambientTemp;");
+    file.print("objectTemp;humCanSat;humExternal;humSCD30;pressCanSat;pressExternal;");
+    file.print("altCanSat;altExternal;accX;accY;accZ;");
+    file.print("rotX;rotY;rotZ;magX;magY;magZ;year;month;day;");
+    file.print("hour;minute;second;numOfSats;latInt;lonInt;latAfterDot;");
+    file.print("lonAfterDot;voltage_shunt;voltage_bus;current_mA;");
+    file.println("voltage_load;co2SCD30;co2CCS811;tvoc;o2Con;a;b;c;d;e;f;g;h;i;j;k;l;r;s;t;u;v;w;");
+    file.close();
+    
+    if (debugLog) {     
+      Serial.println("File header written."); 
+    }
+  } else if (debugLog) {
+    Serial.println("Error opening file!");
   }
 }
